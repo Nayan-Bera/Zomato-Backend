c7ce662de9cae137f3fd3b7fc7e85475998b4c42
Nayan Bera <nayanbera9@gmail.com>
2024-09-09 21:03:19 +0530

    done changes

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require('express')
 
 
 const app = express();
+
+const port =8080;
+
+app.listen(port, (err)=>{
+
+    if(err) console.log("erroe"+ err);
+
+    else console.log("server started succesfully on port"+ port)
+})
