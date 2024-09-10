966fb9cf0d21c454219aff13fc16a3a5851a3962
Nayan Bera <nayanbera9@gmail.com>
2024-09-10 21:40:57 +0530

    add basic apis

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ app.listen(port, (err)=>{
 
     else console.log("server started succesfully on port"+ port)
 })
+//this is middleware use to pass your json structure
+app.use(express.json());
 // create simple get api that gives you some data in response
 
 app.get("/test",(req,res)=>{
-    res.status(200).send("hello from me")
+    res.status(200).send({name:"avik",
+        reason:"meantally sick",age:34
+    })
+})
+
+app.post("/login",(req,res)=>{
+    console.log(req.body)
+res.status(200).send("logged in sucessfully")
+
+
 })
