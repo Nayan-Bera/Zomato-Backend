cef337c2478f3e3eced62a1305232fafa1992d14
Nayan Bera <nayanbera9@gmail.com>
2024-09-18 22:45:41 +0530

    updated menu

--- model.js
+++ model.js
@@ -51,6 +51,30 @@ const Menu =sequelize.define("menu",{
         type:DataTypes.STRING,
         allowNull:false
     },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true
+    },
+
+    password:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }, 
+    
+    phone_number:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        unique:true
+    },
+    createdAt:{
+        type:DataTypes.TIME
+        
+    } ,
+    updatedAt:{
+        type:DataTypes.TIME   
+    }
+
 
 })
 
